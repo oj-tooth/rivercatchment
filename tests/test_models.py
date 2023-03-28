be9330f3680a375ea1f4be472d5f81ae6d6d4932
Ollie Tooth <olivertooth98@gmail.com>
2023-03-28 15:03:47 +0100

    Added tests for daily_min() + daily_max()

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,8 @@
 """Tests for statistics functions within the Model layer."""
 
+import datetime
 import pandas as pd
 import pandas.testing as pdt
-import datetime
 
 
 def test_daily_mean_zeros():
@@ -49,3 +49,49 @@ def test_daily_mean_integers():
 
     # Need to use Pandas testing functions to compare arrays
     pdt.assert_frame_equal(daily_mean(test_input), test_result)
+
+
+def test_daily_max_integers():
+    """Test that max function works for an array of positive integers."""
+    from catchment.models import daily_max
+
+    test_input = pd.DataFrame(
+                     data=[[1, 2],
+                           [3, 4],
+                           [5, 6]],
+                     index=[pd.to_datetime('2000-01-01 01:00'),
+                            pd.to_datetime('2000-01-01 02:00'),
+                            pd.to_datetime('2000-01-01 03:00')],
+                     columns=['A', 'B']
+    )
+    test_result = pd.DataFrame(
+                     data=[[5, 6]],
+                     index=[datetime.date(2000, 1, 1)],
+                     columns=['A', 'B']
+    )
+
+    # Need to use Pandas testing functions to compare arrays
+    pdt.assert_frame_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_integers():
+    """Test that min function works for an array of positive integers."""
+    from catchment.models import daily_min
+
+    test_input = pd.DataFrame(
+                     data=[[1, 2],
+                           [3, 4],
+                           [5, 6]],
+                     index=[pd.to_datetime('2000-01-01 01:00'),
+                            pd.to_datetime('2000-01-01 02:00'),
+                            pd.to_datetime('2000-01-01 03:00')],
+                     columns=['A', 'B']
+    )
+    test_result = pd.DataFrame(
+                     data=[[1, 2]],
+                     index=[datetime.date(2000, 1, 1)],
+                     columns=['A', 'B']
+    )
+
+    # Need to use Pandas testing functions to compare arrays
+    pdt.assert_frame_equal(daily_min(test_input), test_result)
