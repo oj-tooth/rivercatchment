a76155fc796b8ce931c97e70c0155eb41fc454dd
Ollie Tooth <olivertooth98@gmail.com>
2023-03-30 14:41:07 +0100

    Added test_daily_count()

--- tests/test_models.py
+++ tests/test_models.py
@@ -209,3 +209,23 @@ def test_daily_min(test_data, test_index, test_columns, expected_data, expected_
     from catchment.models import daily_min
     pdt.assert_frame_equal(daily_min(pd.DataFrame(data=test_data, index=test_index, columns=test_columns)),
                            pd.DataFrame(data=expected_data, index=expected_index, columns=expected_columns))
+
+@pytest.mark.parametrize(
+    "test_data, test_index, test_columns, expected_data, expected_index, expected_columns",
+    [
+        (
+            [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]],
+            [pd.to_datetime('2000-01-01 01:00'),
+                pd.to_datetime('2000-01-01 02:00'),
+                pd.to_datetime('2000-01-01 03:00')],
+            ['A', 'B', 'C'],
+            [[3, 3, 3]],
+            [datetime.date(2000, 1, 1)],
+            ['A', 'B', 'C']
+        ),
+    ])
+def test_daily_count(test_data, test_index, test_columns, expected_data, expected_index, expected_columns):
+    """Test min function works for array of zeroes and positive integers."""
+    from catchment.models import daily_count
+    pdt.assert_frame_equal(daily_count(pd.DataFrame(data=test_data, index=test_index, columns=test_columns)),
+                           pd.DataFrame(data=expected_data, index=expected_index, columns=expected_columns))
